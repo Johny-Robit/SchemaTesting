969b1208d61138f17ebdec09ea2f3d55de98bfd6
Johny-Robit <jean.robitaille.2@ulaval.ca>
2024-08-06 16:50:43 -0400

    upload3

--- backend/myproject/schema_testing/tests.py
+++ backend/myproject/schema_testing/tests.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
 from .odm2adapter import OdmAdapter
 
-class Odm2AdapterTestCases(TestCase):
-    databases = {'default', 'farm'}  # Ensure both databases are used in the test
+class Odm2AdapterTestCases(TransactionTestCase):
+    databases = {'farm'}  # Ensure both databases are used in the test
 
     def test_insertion(self):
         adapter = OdmAdapter()
